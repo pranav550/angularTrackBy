60385b252fa7b837a059b0c4eaf25519e287ae7a
pranav <pranavverma550@gmail.com>
2019-04-16 07:13:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularngTrackBy';
+
+ students:any[];
+
+ constructor(){
+   this.students=[
+     {
+       studentId:1,
+       name:"amit",
+       gender:"M",
+       age:20
+     },
+     {
+      studentId:2,
+      name:"bbbbb",
+      gender:"M",
+      age:21
+    },
+    {
+      studentId:3,
+      name:"cccccc",
+      gender:"M",
+      age:22
+    },
+   ]
+ }
+
+ getMore():void{
+  this.students=[
+    {
+      studentId:1,
+      name:"amit",
+      gender:"M",
+      age:20
+    },
+    {
+     studentId:2,
+     name:"bbbbb",
+     gender:"M",
+     age:21
+   },
+   {
+     studentId:3,
+     name:"cccccc",
+     gender:"M",
+     age:22
+   },
+   {
+    studentId:4,
+    name:"dddddd",
+    gender:"M",
+    age:23
+  },
+  ]
+ }
+
+ trackByGetMore(index:number, student:any){
+      return student.studentId
+ }
 }
